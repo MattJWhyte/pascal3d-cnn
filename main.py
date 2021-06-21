8f729ae54273031c3a5c73a6f941b5146811ac91
Matthew Whyte <matthewwhyte14@gmail.com>
2021-06-21 09:17:55 +0100

    Debug

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ def train_loop(dataloader, model, loss_fn, optimizer):
         loss.backward()
         optimizer.step()
 
+        print("Attempt")
+
         if batch % 4 == 0:
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
