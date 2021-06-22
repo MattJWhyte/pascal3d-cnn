6c51c365dde9140a8142621f14d50ae8c6147842
Matthew Whyte <matthewwhyte14@gmail.com>
2021-06-22 10:24:40 +0100

    Fixed gpu alloc

--- scripts/eval.py
+++ scripts/eval.py
@@ -30,6 +30,7 @@ def evaluate_model(pth):
     nt = network.Net1()
     nt.load(pth)
     nt.eval()
+    nt.to('cuda' if torch.cuda.is_available() else "cpu")
     dset = PascalDataset(train=False)
     dataloader = DataLoader(dset, batch_size=48)
     n = len(dset)
@@ -40,6 +41,7 @@ def evaluate_model(pth):
         X = X.to('cuda' if torch.cuda.is_available() else "cpu")
         ct += 1
         y = nt(X)
+        y.to("cpu")
         k = thirty_deg_accuracy(y, target)
         theta.append(np.rad2deg(get_angle(y, target)))
         X.detach()
