d8031cf47038a5612d8a989fc6032608a2025eb6
Matthew Whyte <matthewwhyte14@gmail.com>
2021-06-22 19:49:56 +0100

    Reversed channel lengths

--- scripts/network.py
+++ scripts/network.py
@@ -9,17 +9,17 @@ class Net1(nn.Module):
     def __init__(self):
         super(Net1, self).__init__()
         # Input 128 x 128
-        self.conv1 = nn.Conv2d(3, 256, 7) # 122 x 122
-        self.conv2 = nn.Conv2d(256, 256, 7) # 116 x 116 pool 4 = 29 x 29
-        self.conv3 = nn.Conv2d(256, 128, 7, stride=(2,2)) # 12 x 12
-        self.conv4 = nn.Conv2d(128, 64, 5, stride=(2,2)) # 4 x 4
+        self.conv1 = nn.Conv2d(3, 64, 7) # 122 x 122
+        self.conv2 = nn.Conv2d(64, 128, 7) # 116 x 116 pool 4 = 29 x 29
+        self.conv3 = nn.Conv2d(128, 128, 7, stride=(2,2)) # 12 x 12
+        self.conv4 = nn.Conv2d(128, 256, 5, stride=(2,2)) # 4 x 4
 
         # More channels , smaller convolutions
         # Use stride for convolutions instead of max pool
         # Downsample image more before model (128)
 
         # No more than 1024
-        self.fc1 = nn.Linear(4 * 4 * 64, 64)  # 5*5 from image dimension
+        self.fc1 = nn.Linear(4 * 4 * 256, 64)  # 5*5 from image dimension
         self.fc2 = nn.Linear(64, 32)
         self.fc3 = nn.Linear(32, 16)
         self.fc4 = nn.Linear(16, 3)
@@ -40,7 +40,7 @@ class Net1(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
+        x = self.fc4(x)
 
         return x
 
