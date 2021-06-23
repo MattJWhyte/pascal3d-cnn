c5fa244e168cbfd3c5e756cfe8bc17f474ba10be
Matthew Whyte <matthewwhyte14@gmail.com>
2021-06-23 09:24:45 +0100

    Added network

--- scripts/network.py
+++ scripts/network.py
@@ -9,20 +9,23 @@ class Net1(nn.Module):
     def __init__(self):
         super(Net1, self).__init__()
         # Input 128 x 128
-        self.conv1 = nn.Conv2d(3, 64, 7) # 122 x 122
-        self.conv2 = nn.Conv2d(64, 128, 7, stride=(2,2)) # 58 x 58 pool 2 = 29 x 29
-        self.conv3 = nn.Conv2d(128, 128, 7, stride=(2,2)) # 12 x 12
-        self.conv4 = nn.Conv2d(128, 256, 5, stride=(2,2)) # 4 x 4
+        self.conv1 = nn.Conv2d(3, 32, 7)
+        self.conv2 = nn.Conv2d(32, 64, 7)
+        self.conv3 = nn.Conv2d(64, 64, 5, stride=(2,2))
+        self.conv4 = nn.Conv2d(64, 128, 3, stride=(2, 2))
+        self.conv5 = nn.Conv2d(128, 256, 3)
+        self.conv6 = nn.Conv2d(256, 256, 3)
 
         # More channels , smaller convolutions
         # Use stride for convolutions instead of max pool
         # Downsample image more before model (128)
 
         # No more than 1024
-        self.fc1 = nn.Linear(4 * 4 * 256, 64)  # 5*5 from image dimension
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, 16)
-        self.fc4 = nn.Linear(16, 3)
+        self.fc1 = nn.Linear(8 * 8 * 256, 128)  # 5*5 from image dimension
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 32)
+        self.fc4 = nn.Linear(32, 16)
+        self.fc5 = nn.Linear(16, 3)
 
     def forward(self, x):
         # C1
@@ -33,6 +36,10 @@ class Net1(nn.Module):
         x = F.relu(self.conv3(x))
         # C4
         x = F.relu(self.conv4(x))
+        # C5
+        x = F.relu(self.conv5(x))
+        # C6
+        x = F.relu(self.conv6(x))
 
         # Try get it to column vec
 
@@ -40,7 +47,8 @@ class Net1(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        x = self.fc4(x)
+        x = F.relu(self.fc4(x))
+        x = self.fc5(x)
 
         return x
 
