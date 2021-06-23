e8a43f6645807fb8f83d226d441f5d9bd0e37cf9
Matthew Whyte <matthewwhyte14@gmail.com>
2021-06-23 11:05:00 +0100

    Trying stuff

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -35,9 +35,5 @@ class PascalDataset(Dataset):
         img = np.moveaxis(img, -1, 0)
         img = img / 255.0
         out = torch.from_numpy(img).float()
-        print(out)
-        print("LABELS-------------")
-        print(self.labels[idx])
-        print(torch.from_numpy(self.labels[idx]).float())
         return out, torch.from_numpy(self.labels[idx]).float()
 
