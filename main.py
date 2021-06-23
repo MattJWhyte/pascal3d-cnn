03c060ad9226660dc8f8bc0d1ded282b71a1ab4b
Matthew Whyte <matthewwhyte14@gmail.com>
2021-06-23 08:21:27 +0100

    Increase batch size:

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ model = Net1()
 model.to(device)
 
 loss_fn = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters())
+optimizer = torch.optim.SGD(model.parameters(), lr=0.003)
 
 epochs = 20
 for t in range(epochs):
