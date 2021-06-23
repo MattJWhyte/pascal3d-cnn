8fc061af7ed38b360524369f9fa8e42f1537da00
Matthew Whyte <matthewwhyte14@gmail.com>
2021-06-23 09:40:34 +0100

    Added network

--- scripts/network.py
+++ scripts/network.py
@@ -11,7 +11,7 @@ class Net1(nn.Module):
         # Input 128 x 128
         self.conv1 = nn.Conv2d(3, 32, 7)
         self.conv2 = nn.Conv2d(32, 64, 7)
-        self.conv3 = nn.Conv2d(64, 64, 5, stride=(2,2))
+        self.conv3 = nn.Conv2d(64, 64, 7, stride=(2,2))
         self.conv4 = nn.Conv2d(64, 128, 3, stride=(2,2))
         self.conv5 = nn.Conv2d(128, 256, 3)
         self.conv6 = nn.Conv2d(256, 256, 3)
