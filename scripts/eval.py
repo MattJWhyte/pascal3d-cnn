611fd7b832ab5d2d80946da83546f0a96796f722
Matthew Whyte <matthewwhyte14@gmail.com>
2021-06-24 10:44:57 +0100

    Added output eval

--- scripts/eval.py
+++ scripts/eval.py
@@ -1,8 +1,11 @@
+import os
 
 import numpy as np
 import scripts.network
 from scripts.dataset import PascalDataset
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+from network import *
 import torch
 
 # Python file for all evaluation metrics / graphics of nn against ground truth
@@ -30,9 +33,9 @@ def thirty_deg_accuracy(y, target):
     return np.count_nonzero(theta < np.deg2rad(30.0)) / size
 
 
-def evaluate_model(pth):
+def evaluate_model(pth, net):
     # Load model
-    nt = scripts.network.Net1()
+    nt = net()
     nt.load(pth)
     nt.eval()
     nt.to('cuda' if torch.cuda.is_available() else "cpu")
@@ -54,3 +57,45 @@ def evaluate_model(pth):
         acc += k
     return acc/ct, np.median(np.array(theta))
 
+
+def predict_model(pth, net):
+    # Load model
+    nt = net()
+    nt.load(pth)
+    nt.eval()
+    nt.to('cuda' if torch.cuda.is_available() else "cpu")
+    train_dset = PascalDataset()
+    test_dset = PascalDataset(train=False)
+
+    if os.path.exists("results"):
+        os.mkdir("results")
+
+    if os.path.exists("results/predictions"):
+        os.mkdir("results/predictions")
+
+    for i in range(len(train_dset)):
+        y = nt(train_dset[i][0]).numpy()
+        img_name = train_dset.labels[i].replace("/", "-")
+        target = train_dset[i][1].numpy()
+        plt.figure()
+        ax = plt.axes(projection='3d')
+        ax.plot([0,y[0]], [0,y[1]], [0,y[2]], "k-", label="Pred")
+        ax.plot([0, target[0]], [0, target[1]], [0, target[2]], "r-", label="Target")
+        plt.legend()
+        plt.savefig("results/predictions/"+img_name)
+        plt.clf()
+
+    for i in range(len(test_dset)):
+        y = nt(test_dset[i][0]).numpy()
+        img_name = test_dset.labels[i].replace("/", "-")
+        target = test_dset[i][1].numpy()
+        plt.figure()
+        ax = plt.axes(projection='3d')
+        ax.plot([0,y[0]], [0,y[1]], [0,y[2]], "k-", label="Pred")
+        ax.plot([0, target[0]], [0, target[1]], [0, target[2]], "r-", label="Target")
+        plt.legend()
+        plt.savefig("results/predictions/"+img_name)
+        plt.clf()
+
+
+predict_model("models/pascal3d-vp-cnn-net1.pth", Net4)
