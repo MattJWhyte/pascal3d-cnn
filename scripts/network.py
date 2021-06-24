52238e27085d44916ada2c644a290a9e650d6f90
Matthew Whyte <matthewwhyte14@gmail.com>
2021-06-24 10:03:09 +0100

    Added new network

--- scripts/network.py
+++ scripts/network.py
@@ -187,7 +187,7 @@ class Net4(nn.Module):
         self.conv1 = nn.Conv2d(3, 32, 3, stride=(2,2), padding=1)
         self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 32, 3, stride=(3, 3), padding=1)
-        self.bn2 = nn.BatchNorm2d(8)
+        self.bn2 = nn.BatchNorm2d(32)
         self.conv3 = nn.Conv2d(32, 32, 3, stride=(2, 2), padding=1)
         self.bn3 = nn.BatchNorm2d(32)
         self.conv4 = nn.Conv2d(32, 32, 3, stride=(2, 2), padding=1)
