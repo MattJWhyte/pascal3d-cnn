fadc970c64c1d7e6af3801983e07b0db017c2722
Matthew Whyte <matthewwhyte14@gmail.com>
2021-06-24 14:31:50 +0100

    Changed import statements

--- scripts/eval.py
+++ scripts/eval.py
@@ -11,7 +11,9 @@ import torch
 
 
 def distance_elevation_azimuth(xyz):
-    x,y,z = xyz
+    x = xyz[0]
+    y = xyz[1]
+    z = xyz[2]
     theta = np.abs(90-np.rad2deg(np.arccos(z / np.sqrt(x ** 2 + y ** 2 + z ** 2))))
     if z < 0:
         theta *= -1.0
