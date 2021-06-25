e8f27cca16256e9257091b11d747834c55c3ee81
Matthew Whyte <matthewwhyte14@gmail.com>
2021-06-25 12:23:24 +0100

    Switched to net4

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ train_dataloader = DataLoader(train_set, batch_size=128, shuffle=True)
 train_no_shuffle_dataloader = DataLoader(train_set, batch_size=128, shuffle=True)
 test_dataloader = DataLoader(val_set, batch_size=128)
 
-model = Net1()
+model = Net4()
 model.to(device)
 
 loss_fn = nn.MSELoss()
@@ -144,7 +144,7 @@ for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train_loop(train_dataloader, model, loss_fn, optimizer)
     test_loop(test_dataloader, model, loss_fn)
-    model.save("models/pascal3d-vp-cnn-net1.pth")
+    model.save("models/pascal3d-vp-cnn-net4.pth")
     plt.plot([i for i in range(1,t+2)], train_acc_ls, 'r-', label="Train acc.")
     plt.plot([i for i in range(1, t + 2)], train_loss_ls, 'r--', label="Train loss")
     plt.plot([i for i in range(1, t + 2)], test_acc_ls, 'b-', label="Test acc.")
