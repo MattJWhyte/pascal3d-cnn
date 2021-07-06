6895eefe63f70d417f7ec481de1414d79b427a27
Matthew Whyte <matthewwhyte14@gmail.com>
2021-07-06 14:54:56 +0100

    Reverted eval_model file

--- eval_model.py
+++ eval_model.py
@@ -10,10 +10,4 @@ model_name = sys.argv[1].lower()
 width = int(sys.argv[2])
 height = int(sys.argv[3])
 
-train_mat, test_mat = eval.get_model_thirty_deg_vector("models/pascal3d-vp-cnn-"+model_name+".pth", MODEL[model_name], (width,height))
-
-print(test_mat)
-
-np.save("results/{}/train_acc_mat.npy".format(model_name), train_mat)
-np.save("results/{}/test_acc_mat.npy".format(model_name), test_mat)
-
+eval.predict_model("models/pascal3d-vp-cnn-"+model_name+".pth", MODEL[model_name], model_name, (width,height))
