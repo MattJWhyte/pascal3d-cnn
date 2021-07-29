20424a15bdc1c3b4b22a80c645ca5dcb8f8d59ef
Matthew Whyte <matthewwhyte14@gmail.com>
2021-07-29 16:02:31 +0100

    Fixed save/load image function

--- eval_model.py
+++ eval_model.py
@@ -10,12 +10,14 @@ model_name = sys.argv[1].lower()
 width = int(sys.argv[2])
 height = int(sys.argv[3])
 
-#eval.predict_model("models/pascal3d-vp-cnn-"+model_name+".pth", MODEL[model_name], model_name, (width,height))
+eval.predict_model("models/pascal3d-vp-cnn-"+model_name+"-test.pth", MODEL[model_name], model_name, (width,height))
 
+
+'''
 tr_vec, tr_cat, te_vec, te_cat = eval.get_model_thirty_deg_vector("models/pascal3d-vp-cnn-"+model_name+".pth",
                                                                   MODEL[model_name], (width,height))
 
 np.save("results/{}/train-acc.npy".format(model_name), tr_vec)
 np.save("results/{}/train-cat.npy".format(model_name), tr_cat)
 np.save("results/{}/test-acc.npy".format(model_name), te_vec)
-np.save("results/{}/test-cat.npy".format(model_name), te_cat)
+np.save("results/{}/test-cat.npy".format(model_name), te_cat)'''
