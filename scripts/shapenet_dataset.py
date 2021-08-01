e496726659ee3057df012bd32c07706b9cc0b121
Matthew Whyte <matthewwhyte14@gmail.com>
2021-08-01 19:59:50 +0100

    Added color jitter transform

--- scripts/shapenet_dataset.py
+++ scripts/shapenet_dataset.py
@@ -70,8 +70,7 @@ class ShapeNetDataset(Dataset):
             transform = transforms.Compose([
                 transforms.Resize(self.size),
                 transforms.ToTensor(),
-                RandomResizedCrop(self.size, scale=(0.4, 1.0), ratio=(0.7, 2.0)),
-                ColorJitter(brightness=0.3, hue=0.3, saturation=0.3, contrast=0.3)
+                RandomResizedCrop(self.size, scale=(0.4, 1.0), ratio=(0.7, 1.6))
             ])
             t_img = transform(img)
         else:
@@ -91,8 +90,7 @@ class ShapeNetDataset(Dataset):
             t_back_img.save(os.path.join(SUN_DIR,"temp",img_name.replace("/","-")))
             transform = transforms.Compose([
                 transforms.ToTensor(),
-                RandomResizedCrop(self.size, scale=(0.4, 1.0), ratio=(0.7, 2.0)),
-                ColorJitter(brightness=0.3, hue=0.3, saturation=0.3, contrast=0.3)
+                RandomResizedCrop(self.size, scale=(0.4, 1.0), ratio=(0.7, 1.6))
             ])
             t_img = transform(t_back_img)
         return t_img, torch.from_numpy(self.labels[idx]).float()
