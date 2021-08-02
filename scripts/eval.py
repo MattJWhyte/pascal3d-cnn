9c6135ab656066c31c12f236dcb73a5034cd8e5f
Matthew Whyte <matthewwhyte14@gmail.com>
2021-08-02 17:01:50 +0100

    Fixed debug plots

--- scripts/eval.py
+++ scripts/eval.py
@@ -56,11 +56,12 @@ def thirty_deg_accuracy_vector(y, target):
     size = float(theta.shape[0])
     return theta < np.deg2rad(30.0)
 
+
 def thirty_deg_accuracy_vector_full(y, target):
-    #y = y.numpy()
-    #target = target.detach().numpy()
-    y = np.repeat(np.array([[1,0,0]]), y.shape[0], axis=0)
-    target = np.repeat(np.array([[0,1,1]]), target.shape[0], axis=0)
+    y = y.numpy()
+    target = target.detach().numpy()
+    #y = np.repeat(np.array([[1,0,0]]), y.shape[0], axis=0)
+    #target = np.repeat(np.array([[0,1,1]]), target.shape[0], axis=0)
     y_norm = np.linalg.norm(y, axis=1)
     target_norm = np.linalg.norm(target, axis=1)
     norm = y_norm * target_norm.T
