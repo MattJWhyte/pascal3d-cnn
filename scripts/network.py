1a5ecae236397173742ce3a04019c87187f321f0
Matthew Whyte <matthewwhyte14@gmail.com>
2021-08-02 23:49:34 +0100

    Changed comments

--- scripts/network.py
+++ scripts/network.py
@@ -218,16 +218,18 @@ class Net5(nn.Module):
     def __init__(self):
         super(Net5, self).__init__()
         # Input 224 x 224
-        self.conv1 = nn.Conv2d(3, 32, 3, stride=(2,2), padding=1)
+        self.conv1 = nn.Conv2d(3, 32, 3, stride=(2,2), padding=1) # 112
         self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 64, 3, stride=(3, 3), padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=(2, 2), padding=1) # 56
         self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 128, 3, stride=(2, 2), padding=1)
+        self.conv3 = nn.Conv2d(64, 128, 3, stride=(2, 2), padding=1) # 28
         self.bn3 = nn.BatchNorm2d(128)
-        self.conv4 = nn.Conv2d(128, 256, 3, stride=(2, 2), padding=1)
+        self.conv4 = nn.Conv2d(128, 256, 3, stride=(2, 2), padding=1) # 14
         self.bn4 = nn.BatchNorm2d(256)
-        self.conv5 = nn.Conv2d(256, 256, 3, stride=(2, 2), padding=1)
+        self.conv5 = nn.Conv2d(256, 256, 3, stride=(2, 2), padding=1) # 7
         self.bn5 = nn.BatchNorm2d(256)
+        self.conv6 = nn.Conv2d(256, 256, 3, stride=(2, 2), padding=1) # 4
+        self.bn6 = nn.BatchNorm2d(256)
 
         # More channels , smaller convolutions
         # Use stride for convolutions instead of max pool
@@ -235,7 +237,7 @@ class Net5(nn.Module):
 
         # No more than 1024
         self.fc1 = nn.Linear(3 * 3 * 256, 128)  # 5*5 from image dimension
-        self.bn6 = nn.BatchNorm1d(128)
+        self.bn7 = nn.BatchNorm1d(128)
         self.fc2 = nn.Linear(128, 3)
 
     def forward(self, x):
@@ -249,10 +251,12 @@ class Net5(nn.Module):
         x = F.relu(self.bn4(self.conv4(x)))
         # C5
         x = F.relu(self.bn5(self.conv5(x)))
+        # C6
+        x = F.relu(self.bn6(self.conv6(x)))
         # Try get it to column vec
 
         x = torch.flatten(x, 1)    # flatten all dimensions except the batch dimension
-        x = F.silu(self.bn6(self.fc1(x)))
+        x = F.silu(self.bn7(self.fc1(x)))
         x = F.silu(self.fc2(x))
 
         return x
