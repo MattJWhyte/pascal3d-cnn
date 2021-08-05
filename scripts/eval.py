4f26574ac7b41a04fdcbf595103f1e51fdc96581
Matthew Whyte <matthewwhyte14@gmail.com>
2021-08-05 13:44:57 +0100

    Remove print statement

--- scripts/eval.py
+++ scripts/eval.py
@@ -109,7 +109,6 @@ def get_accuracy_vector(pth, net, cat):
         X = X.to('cuda' if torch.cuda.is_available() else "cpu")
         y = nt(X).detach().cpu()
         k = thirty_deg_accuracy(y, target)
-        print(k)
         X.detach()
         del X
         torch.cuda.empty_cache()
